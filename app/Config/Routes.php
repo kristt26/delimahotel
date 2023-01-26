352c5c19493fc6f5b97283bc05fc18ea795599f2
Kristt26 <Kristt26@gmail.com>
2023-01-26 09:41:35 +0900

    df

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -30,6 +30,10 @@ $routes->set404Override();
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
+$routes->get('auth', 'Auth::index');
+$routes->group('dashboard', function($routes){
+    $routes->get('', 'Admin\Dashboard::index');
+}); 
 
 /*
  * --------------------------------------------------------------------
